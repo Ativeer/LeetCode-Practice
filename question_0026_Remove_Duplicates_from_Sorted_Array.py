31e7881dfebed21041b594d92c596c2deec3cc19
Ativeer <2ativeer@gmail.com>
2021-06-12 19:49:53 -0400

    In PLace removal of Duplicate Elements in Sorted Manner

---
+++ question_0026_Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        count = 0
+        for i in range(1, len(nums)):
+            if nums[i-1] != nums[i]:
+                count += 1
+                nums[count] = nums[i]
+        return count + 1
