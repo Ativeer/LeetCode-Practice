c886fe7f1f7e46912ecb438cfc3f85392c086df6
Ativeer <2ativeer@gmail.com>
2021-07-14 17:15:07 -0400

    Using Joins

--- question1549_recent_order_for_every_product.sql
+++ question1549_recent_order_for_every_product.sql
@@ -5,3 +5,16 @@ select p.product_name, o.product_id, o.order_id, o.order_date from
 join products p on o.product_id=p.product_id
 where rnk=1
 order by p.product_name, o.order_id
+
+
+# Using Join
+
+select p.product_name, p.product_id, o1.order_id, o1.order_date
+from orders o1 join products p on p.product_id=o1.product_id
+WHERE
+(p.product_id, o1.order_date)
+IN
+(select product_id, max(order_date) as m from Orders
+group by product_id)
+
+order by p.product_name, o1.order_id
