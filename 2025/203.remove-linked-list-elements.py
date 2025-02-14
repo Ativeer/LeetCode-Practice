6ea3b82306b739915cf9d155bfcfa6300940152a
Ativeer <2ativeer@gmail.com>
2025-02-13 19:56:09 -0500

    Use a dummy linkedlist to store the answer.

---
+++ 2025/203.remove-linked-list-elements.py
@@ -0,0 +1,78 @@
+#
+# @lc app=leetcode id=203 lang=python3
+#
+# [203] Remove Linked List Elements
+#
+# https://leetcode.com/problems/remove-linked-list-elements/description/
+#
+# algorithms
+# Easy (50.92%)
+# Likes:    8562
+# Dislikes: 260
+# Total Accepted:    1.3M
+# Total Submissions: 2.5M
+# Testcase Example:  '[1,2,6,3,4,5,6]\n6'
+#
+# Given the head of a linked list and an integer val, remove all the nodes of
+# the linked list that has Node.val == val, and return the new head.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: head = [1,2,6,3,4,5,6], val = 6
+# Output: [1,2,3,4,5]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: head = [], val = 1
+# Output: []
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: head = [7,7,7,7], val = 7
+# Output: []
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in the list is in the range [0, 10^4].
+# 1 <= Node.val <= 50
+# 0 <= val <= 50
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        if not head: return
+
+        prev = ListNode()
+        ans = prev
+        while head.next:
+            if head.val != val:
+                prev.next = head
+                prev = prev.next
+            head = head.next
+        # check for the last value
+        if head.val == val:
+            prev.next = None
+        else:
+            prev.next = head
+        return ans.next
+    
+
+# @lc code=end
+
