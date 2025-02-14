98504ca01795c4b5e5402204d68f6516b04460f8
Ativeer <2ativeer@gmail.com>
2025-02-13 19:08:46 -0500

    Preorder using recurrsion and iterative method.

---
+++ 2025/144.binary-tree-preorder-traversal.py
@@ -0,0 +1,115 @@
+#
+# @lc app=leetcode id=144 lang=python3
+#
+# [144] Binary Tree Preorder Traversal
+#
+# https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+#
+# algorithms
+# Easy (72.06%)
+# Likes:    8261
+# Dislikes: 216
+# Total Accepted:    1.9M
+# Total Submissions: 2.6M
+# Testcase Example:  '[1,null,2,3]'
+#
+# Given the root of a binary tree, return the preorder traversal of its nodes'
+# values.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: root = [1,null,2,3]
+# 
+# Output: [1,2,3]
+# 
+# Explanation:
+# 
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+# 
+# Output: [1,2,4,5,6,7,3,8,9]
+# 
+# Explanation:
+# 
+# 
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: root = []
+# 
+# Output: []
+# 
+# 
+# Example 4:
+# 
+# 
+# Input: root = [1]
+# 
+# Output: [1]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+# 
+# 
+# 
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        '''
+        recurssion
+        '''
+        # ans = []
+        # def dfs(node):
+        #     if not node: return
+        #     ans.append(node.val)
+        #     dfs(node.left)
+        #     dfs(node.right)
+        # dfs(root)
+        # return ans
+        
+        '''
+        Iterative
+        '''
+        ans = []
+        if not root: return ans
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            ans.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return ans
+            
+
+
+
+
+# @lc code=end
+
