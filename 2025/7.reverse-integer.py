8ecf62ed2c7f900e7f44e6edb280fddcace3e946
Ativeer <2ativeer@gmail.com>
2025-02-20 22:39:19 -0500

    Use 64 bit integer to check if the number crossed 2 to the power 31 at any point.

---
+++ 2025/7.reverse-integer.py
@@ -0,0 +1,72 @@
+#
+# @lc app=leetcode id=7 lang=python3
+#
+# [7] Reverse Integer
+#
+# https://leetcode.com/problems/reverse-integer/description/
+#
+# algorithms
+# Medium (29.75%)
+# Likes:    13825
+# Dislikes: 13723
+# Total Accepted:    3.8M
+# Total Submissions: 12.9M
+# Testcase Example:  '123'
+#
+# Given a signed 32-bit integer x, return x with its digits reversed. If
+# reversing x causes the value to go outside the signed 32-bit integer range
+# [-2^31, 2^31 - 1], then return 0.
+# 
+# Assume the environment does not allow you to store 64-bit integers (signed or
+# unsigned).
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: x = 123
+# Output: 321
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: x = -123
+# Output: -321
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: x = 120
+# Output: 21
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# -2^31 <= x <= 2^31 - 1
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def reverse(self, x: int) -> int:
+        num = abs(x)
+        ans = 0
+        
+        while num > 0:
+            rem = num % 10
+            ans *= 10
+            ans += rem
+            if ans > (2**31)-1:
+                return 0
+
+            num = num // 10
+        
+        if x > 0: return ans
+        return -ans
+# @lc code=end
+
