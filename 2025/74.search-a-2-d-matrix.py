44bec1287a54601e17352594e82cdad7396f99f7
Ativeer <2ativeer@gmail.com>
2025-02-27 21:47:41 -0500

    Not happy with this solution. Maybe I am missing something? idk.

---
+++ 2025/74.search-a-2-d-matrix.py
@@ -0,0 +1,79 @@
+#
+# @lc app=leetcode id=74 lang=python3
+#
+# [74] Search a 2D Matrix
+#
+# https://leetcode.com/problems/search-a-2d-matrix/description/
+#
+# algorithms
+# Medium (51.65%)
+# Likes:    16500
+# Dislikes: 440
+# Total Accepted:    2.2M
+# Total Submissions: 4.3M
+# Testcase Example:  '[[1,3,5,7],[10,11,16,20],[23,30,34,60]]\n3'
+#
+# You are given an m x n integer matrix matrix with the following two
+# properties:
+# 
+# 
+# Each row is sorted in non-decreasing order.
+# The first integer of each row is greater than the last integer of the
+# previous row.
+# 
+# 
+# Given an integer target, return true if target is in matrix or false
+# otherwise.
+# 
+# You must write a solution in O(log(m * n)) time complexity.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+# Output: false
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# m == matrix.length
+# n == matrix[i].length
+# 1 <= m, n <= 100
+# -10^4 <= matrix[i][j], target <= 10^4
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        new_array = []
+        for row in matrix:
+            new_array += row
+        
+        low = 0
+        high = len(new_array)-1
+
+        while low <= high:
+            mid = low + (high-low)//2
+            if new_array[mid] == target:
+                return True
+            
+            if new_array[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return False
+    
+# @lc code=end
+
