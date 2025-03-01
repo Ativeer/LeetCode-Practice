a463a82511f24a01fe232fbd6f311a4f31e224cf
Ativeer <2ativeer@gmail.com>
2025-03-01 15:05:41 -0500

    Use queue to store entire preorder list.

---
+++ 2025/105.construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,80 @@
+#
+# @lc app=leetcode id=105 lang=python3
+#
+# [105] Construct Binary Tree from Preorder and Inorder Traversal
+#
+# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+#
+# algorithms
+# Medium (65.91%)
+# Likes:    15667
+# Dislikes: 558
+# Total Accepted:    1.5M
+# Total Submissions: 2.2M
+# Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+#
+# Given two integer arrays preorder and inorder where preorder is the preorder
+# traversal of a binary tree and inorder is the inorder traversal of the same
+# tree, construct and return the binary tree.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+# Output: [3,9,20,null,null,15,7]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: preorder = [-1], inorder = [-1]
+# Output: [-1]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= preorder.length <= 3000
+# inorder.length == preorder.length
+# -3000 <= preorder[i], inorder[i] <= 3000
+# preorder and inorder consist of unique values.
+# Each value of inorder also appears in preorder.
+# preorder is guaranteed to be the preorder traversal of the tree.
+# inorder is guaranteed to be the inorder traversal of the tree.
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        pos = {}
+        
+        for i in range(len(inorder)):
+            pos[inorder[i]] = i
+        
+        q = deque(preorder)
+
+        def buildTree(start, end):
+            if start > end: 
+                return None
+            preorder_val = q.popleft()
+            node = TreeNode(preorder_val)
+
+            node.left = buildTree(start, pos[preorder_val]-1)
+            node.right = buildTree(pos[preorder_val]+1, end)
+            return node
+        
+        return buildTree(0, len(preorder)-1)
+
+
+# @lc code=end
+
